864c9b3af2fa1653ac0ebfce4810b0a8b1dcf549
Moon Coder <127067477+mooncoder323@users.noreply.github.com>
2023-06-12 19:37:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
-app.use(cors({
-    origin: 'https://front-proxy.vercel.app'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => res.json("SERVER is WORKING"))
+app.get("/", (req, res) => res.json("SERVER is WORKING"));
+
 app.post("/check-ip", async (req, res) => {
   const checkIpRisk = async (ip) => {
     const endpoint = `https://scamalytics.com/ip/${ip}`;
@@ -143,7 +146,7 @@ app.post("/check-ip", async (req, res) => {
         "An error occurred while processing your request. Please try again.",
     });
   }
-});
+}); 
 
 app.listen(port, () => {
   console.log(`IP Checker app listening at http://localhost:${port}`);
